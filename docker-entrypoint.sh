895775352ddf84d8c447a345172feb2e65ce3b27
Lorenzo Boccaccia <lorenzo.boccaccia@gmail.com>
2019-01-04 12:11:52 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+cleanup() {
+    echo "Container stopped, performing cleanup..."
+    cockroach quit --decommission --insecure 
+}
+
+trap 'cleanup' SIGTERM
+
 echo starting server with   $(  nslookup tasks.cockroachdb | grep Address | tail -n +2 | tail -n 3 | sed "s/Address: /,/" | sed "1s/^,/--join=/" | tr -d '\r\n'  )  2>&1
 
 
-exec /cockroach/cockroach start --insecure --logtostderr   $(  nslookup tasks.cockroachdb | grep Address | tail -n +2 | tail -n 3 | sed "s/Address: /,/" | sed "1s/^,/--join=/" | tr -d '\r\n'  ) 2>&1
+/cockroach/cockroach start --insecure --logtostderr   $(  nslookup tasks.cockroachdb | grep Address | tail -n +2 | tail -n 3 | sed "s/Address: /,/" | sed "1s/^,/--join=/" | tr -d '\r\n'  ) 2>&1 &
 
-	
+wait $!
 
 
 
