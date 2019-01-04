93b840ce082b624b90dcbe5d1b55d65cdbc7df54
Lorenzo Boccaccia <lorenzo.boccaccia@gmail.com>
2019-01-04 11:34:05 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,8 @@
 
 echo "starting server"
 
-exec /cockroach/cockroach "$@"
+exec /cockroach/cockroach "$@"  $(  nslookup tasks.cockroachdb | grep Address | tail -n +2 | tail -n 3 | sed "s/Address: /,/" | sed "1s/^,/--join=/" | tr -d '\r\n'  )
+
 	
 
 
